e8619806db434eba8bbb0bce5ebf3c264f6ffaaa
Airshock <gelagela2222@gmail.com>
2025-11-14 12:47:20 +0400

    implemented posthog into the project

--- next.config.ts
+++ next.config.ts
@@ -1,10 +1,24 @@
-import type { NextConfig } from "next";
+import type { NextConfig } from 'next'
 
 const nextConfig: NextConfig = {
-    reactCompiler: true,
-    experimental: {
-        turbopackFileSystemCacheForDev: true
-    }
-};
+  reactCompiler: true,
+  experimental: {
+    turbopackFileSystemCacheForDev: true
+  },
+  async rewrites() {
+    return [
+      {
+        source: '/ingest/static/:path*',
+        destination: 'https://eu-assets.i.posthog.com/static/:path*',
+      },
+      {
+        source: '/ingest/:path*',
+        destination: 'https://eu.i.posthog.com/:path*',
+      },
+    ]
+  },
+  // This is required to support PostHog trailing slash API requests
+  skipTrailingSlashRedirect: true,
+}
 
-export default nextConfig;
+export default nextConfig
